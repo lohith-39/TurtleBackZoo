f5db94f97e90ea6527aca892f7f4f08fd70b19e0
Tamma Lohith Reddy <68261390+lohith-39@users.noreply.github.com>
2025-01-09 09:07:01 -0500

    Add files via upload

---
+++ tbz/src/test/java/com/app/tbz/TbzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tbz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
